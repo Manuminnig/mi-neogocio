00bcd9a9fa169a383d0e897ea4ad90d2f61ad32e
Manu Minnig <manuminnig@gmail.com>
2023-10-05 17:14:21 -0300

    Testing con base de datos simulada

--- src/productos/poductos.controller.spec.ts
+++ src/productos/poductos.controller.spec.ts
@@ -1,34 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductosController } from './productos.controller';
 import { ProductosService } from './productos.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Producto } from './entities/producto.entity';
-import { Oferta } from 'src/ofertas/entities/oferta.entity';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 describe('ProductosController', () => {
   let controller: ProductosController;
+  const mock={ 
+    productos:[
+    { 
+      id: 1,
+      name: "Coca-Cola", 
+      description: "Gaseosa 2.5L",
+      price: 700,
+    },
+    { 
+      id: 2,
+      name: "Spritte", 
+      description: "Gaseosa 2.5L",
+      price: 700,
+    }
+    ],
+    getAllProductos() { return this.productos }
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductosController],
       providers: [ProductosService],
-      imports: [
-        TypeOrmModule.forRoot({
-          type: 'postgres',
-          host: process.env.DATABASE_HOST,
-          port: Number(process.env.DATABASE_PORT),
-          username: process.env.DATABASE_USERNAME,
-          password: process.env.DATABASE_PASSWORD,
-          database: process.env.DATABASE_NAME,
-          entities: [Producto, Oferta],
-          synchronize: true,
-        }),
-        TypeOrmModule.forFeature([Producto]),
-      ]
-    }).compile();
+      
+    }).overrideProvider(ProductosService).useValue(mock).compile();
 
     controller = module.get<ProductosController>(ProductosController);
   });
